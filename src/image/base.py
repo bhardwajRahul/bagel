8adbe01e691fd709238beb344848cdd52ca97577
Shouheng Yi <shouhengyi@gmail.com>
2025-10-07 14:23:13 -0700

    Added ImageDataset and its ROS1 implementation

---
+++ src/image/base.py
@@ -0,0 +1,94 @@
+"""An abstract base class for image datasets."""
+
+import abc
+from collections.abc import Iterator
+
+from PIL import Image
+
+from src.source.base import SourceFactory
+from src.topic.base import TopicRegistry
+
+
+class NotImageTopicError(Exception):
+    """Raised when a topic is not an image topic."""
+
+
+class ImageDataset(abc.ABC):
+    """An abstract base class for image datasets.
+
+    This ABC provides a common contract for implementing extractor of images,
+    e.g., camera images.
+
+    Note:
+        Constructors of all subclasses must only accept primitive types (e.g.,
+        str, int, bool). This ensures instances can be reliably serialized and
+        recreated via dependency injection.
+
+    """
+
+    @property
+    @abc.abstractmethod
+    def image_type_name(self) -> str:
+        """The native type name of image topics."""
+
+    @abc.abstractmethod
+    def _images(
+        self,
+        data_source: object,
+        topics: list[str],
+        start_seconds_inclusive: float | None,
+        end_seconds_inclusive: float | None,
+    ) -> Iterator[tuple[str, float, Image.Image]]:
+        """Return an iterator over images from the data source.
+
+        Args:
+            data_source (object): The data source to extract images from.
+            topics (list[str]): The list of topics to extract.
+            start_seconds_inclusive (float | None): The start time seconds (inclusive).
+                If None, starts from the beginning.
+            end_seconds_inclusive (float | None): The end time seconds (inclusive).
+                If None, reads until the end.
+
+        Yields:
+            Iterator[tuple[str, float, Image.Image]]: Yielding tuples of topic,
+                timestamp in seconds, and the PIL.Image object.
+
+        """
+
+    def images(  # noqa: D417
+        self,
+        factory: SourceFactory,
+        registry: TopicRegistry,
+        topics: list[str] | None = None,
+        start_seconds: float | None = None,
+        end_seconds: float | None = None,
+    ) -> Iterator[tuple[str, float, Image.Image]]:
+        """Return an iterator over images from the data source.
+
+        Args:
+            factory (SourceFactory): The source factory for creating the data source.
+            registry (TopicRegistry): The topic registry for looking up topic schemas.
+            topics (list[str]): The list of topics to extract.
+            start_seconds_inclusive (float | None): The start time seconds (inclusive).
+                If None, starts from the beginning.
+            end_seconds_inclusive (float | None): The end time seconds (inclusive).
+                If None, reads until the end.
+
+        Raises:
+            NotImageTopicError: Raised when a topic is not an image topic.
+
+        Yields:
+            Iterator[tuple[str, float, Image.Image]]: Yielding tuples of topic,
+                timestamp in seconds, and the PIL.Image object.
+
+        """
+        data_source = factory.build()
+        image_topics = [
+            topic
+            for topic in registry.available_topics(data_source)
+            if registry.native_type_name(topic, data_source) == self.image_type_name
+        ]
+        none_image_topics = [topic for topic in topics or [] if topic not in image_topics]
+        if none_image_topics:
+            raise NotImageTopicError(none_image_topics)
+        yield from self._images(data_source, topics or image_topics, start_seconds, end_seconds)
---
+++ src/image/ros1/bag.py
@@ -0,0 +1,48 @@
+"""An image dataset for ROS1 bags."""
+
+from collections.abc import Iterator
+
+import cv2
+import genpy
+import rosbag
+from cv_bridge import CvBridge
+from PIL import Image
+
+from src.di import module
+from src.image import base
+
+bridge = CvBridge()
+
+
+class ImageDataset(base.ImageDataset):
+    """An image dataset for ROS1 bags."""
+
+    @property
+    def image_type_name(self) -> str:
+        """ROS1 image type name."""
+        return "sensor_msgs/Image"
+
+    def _images(
+        self,
+        data_source: rosbag.Bag,
+        topics: list[str],
+        start_seconds_inclusive: float | None,
+        end_seconds_inclusive: float | None,
+    ) -> Iterator[tuple[str, float, Image.Image]]:
+        """Return an iterator of topic name, timestamp in seconds, and images."""
+        messages = data_source.read_messages(
+            topics,
+            genpy.Time.from_sec(start_seconds_inclusive) if start_seconds_inclusive else None,
+            genpy.Time.from_sec(end_seconds_inclusive) if end_seconds_inclusive else None,
+        )
+
+        for topic, message, timestamp in messages:
+            cv_image = bridge.imgmsg_to_cv2(message, desired_encoding="passthrough")
+            if message.encoding.lower() in ("bgr8", "bgr16"):
+                cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+            yield topic, timestamp.to_sec(), Image.fromarray(cv_image)
+
+
+def register() -> None:
+    """Register module for dependency injection."""
+    module.global_registry[__name__] = ImageDataset
